8c64ae1767621240e67fbb96a5cd98b93516f5d9
greatt8696 <greatt8696@gmail.com>
2022-10-11 05:01:33 +0900

    msg

--- src/components/MsgSlot.js
+++ src/components/MsgSlot.js
@@ -6,7 +6,7 @@ const MsgSlot = () => {
   const msgs = useSelector((state) => state.msgReducer.msgs);
 
   return (
-    <div className="fixed bottom-0 right-0 w-56 h-3/4 flex flex-col-reverse py-5 mx-5 gap-2 z-0">
+    <div className="fixed bottom-0 right-0 w-56 h-[15px] flex flex-col-reverse py-5 mx-5 gap-2 z-0">
       {msgs.map((msg, idx) => (
         <Msg msg={msg} key={idx}></Msg>
       ))}
